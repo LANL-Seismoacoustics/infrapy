fixing issues with joint likelihood calculation and PGMs

infrapy/propagation/infrasound.py
@@ -11,6 +11,7 @@ import pickle
 import imp
 import time
 import itertools
+import warnings
 
 import numpy as np
 
@@ -31,6 +32,7 @@ from ..utils import prog_bar
 from ..utils import skew_norm
 
 np.seterr(over='ignore', divide='ignore')
+warnings.simplefilter(action='ignore', category=FutureWarning)
 
 # ############################ #
 #      General (Canonical)     #
@@ -108,9 +110,9 @@ class PathGeometryModel(object):
 
         if len(np.atleast_1d(rng)) == 1:
             n_az = find_azimuth_bin(az, self.az_bin_cnt)
-            fit_rcel_wts = np.array([f(rng_eval) for f in self.rcel_wts[n_az]])
-            fit_rcel_mns = np.array([f(rng_eval) for f in self.rcel_mns[n_az]])
-            fit_rcel_vrs = np.array([f(rng_eval) for f in self.rcel_vrs[n_az]])
+            fit_rcel_wts = np.array([func(rng_eval) for func in self.rcel_wts[n_az]])
+            fit_rcel_mns = np.array([func(rng_eval) for func in self.rcel_mns[n_az]])
+            fit_rcel_vrs = np.array([func(rng_eval) for func in self.rcel_vrs[n_az]])
             result = np.sum(fit_rcel_wts / fit_rcel_vrs * norm.pdf((rcel - fit_rcel_mns) / fit_rcel_vrs))
         else:
             mn = np.empty((len(rng_eval), 3))
@@ -119,11 +121,11 @@ class PathGeometryModel(object):
 
             az_indices = find_azimuth_bin(az, self.az_bin_cnt)
             for n_az in range(self.az_bin_cnt):
-                mask = [az_indices==n_az]
+                mask = [az_indices == n_az]
                 if np.any(mask):
-                    mn[mask] = np.array([f(rng_eval[mask]) for f in self.rcel_mns[n_az]]).T
-                    vr[mask] = np.array([f(rng_eval[mask]) for f in self.rcel_vrs[n_az]]).T
-                    wt[mask] = np.array([f(rng_eval[mask]) for f in self.rcel_wts[n_az]]).T
+                    mn[mask] = np.array([func(rng_eval[mask]) for func in self.rcel_mns[n_az]]).T
+                    vr[mask] = np.array([func(rng_eval[mask]) for func in self.rcel_vrs[n_az]]).T
+                    wt[mask] = np.array([func(rng_eval[mask]) for func in self.rcel_wts[n_az]]).T
 
             result = np.sum(wt / vr * norm.pdf((np.array([rcel] * 3).T - mn) / vr), axis=1)
         return result

infrapy/propagation/likelihoods.py
@@ -556,7 +556,7 @@ class SeismicDetection(object):
 #      Infrasound Likelihoods      #
 # ################################ #
 def joint_pdf(lat, lon, t, det_list, path_geo_model=None, prog_step=0):
-    result = np.array([det.pdf(lat, lon, t, path_geo_model=None) for det in det_list if type(det) == InfrasoundDetection]).prod(axis=0)
+    result = np.array([det.pdf(lat, lon, t, path_geo_model=path_geo_model) for det in det_list if type(det) == InfrasoundDetection]).prod(axis=0)
     result *= np.array([det.pdf(lat, lon, t) for det in det_list if type(det) == SeismicDetection]).prod(axis=0)
     prog_bar.increment(n=prog_step)
     return result
@@ -575,6 +575,13 @@ def marginal_spatial_pdf(lat, lon, det_list, path_geo_model=None, prog_step=0, r
     from IPython import embed
 
     if 3**infr_cnt > resol:
+        
+        if path_geo_model:
+            print("Computing numerical integral with PGM")
+        else:
+            print("Computing numerical integral without PGM")
+
+
         def temp(la, lo):
             infr_rngs = np.array([sph_proj.inv(det.longitude, det.latitude, lo, la)[2] / 1000.0 for det in infr_det_list])
             infr_t1 = max(np.array([det.peakF_UTCtime - np.timedelta64(int(infr_rngs[n] / 0.2 * 1e3), 'ms') for n, det in enumerate(infr_det_list)]))