7acc3b09b6b8f6462da0171fcea6a50e22812c6e
Jeremy Webster <jwebster@lanl.gov>
2021-04-29 10:51:15 -0600

    updated infrapy_env and create update_infrapy to make updating from git AND updating packages via the yml easier

--- update_infrapy.sh
+++ update_infrapy.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+echo "Checking github for updates..."
 git pull
+echo 
+echo "Checking for updates in the yml file"
 conda env update --name infrapy_env_new --file infrapy_env.yml --prune
