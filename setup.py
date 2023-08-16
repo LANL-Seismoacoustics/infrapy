adcd79060ab5ccac075fd9cc7f0d771a08e7c5b6
Jeremy David Webster <jwebster@lanl.gov>
2023-08-16 13:00:49 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from distutils.core import setup
 
 setup(name = "infrapy",
       license='LANL-MIT',
-      version = '0.4.0.4',
+      version = '0.4.0.5',
       description = "A tool for processing infrasound and seismic array data.",
       keywords=['infrasound', 'geophysics', 'seismic', 'array'],
       author = "LANL Seismoacoustics Infrasound (LANL-SA) Team",
